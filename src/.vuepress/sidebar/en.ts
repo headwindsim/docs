3ffa38403bff445563c901047cbd2a6665aba77e
Denis Freund <info@denis-freund.de>
2023-06-02 15:52:10 +0200

    adjust sidebar

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,21 +1,6 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const enSidebar = sidebar({
-  "/": [
-    "",
-    {
-      text: "Demo",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "Docs",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
-    "slides",
-  ],
+  "/demo/": "structure",
+  "/guide/": "structure",
 });
