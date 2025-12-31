02152df9b5f231693d7cd9cec1fd1b3f660d870e
Denis Freund <info@denis-freund.de>
2025-12-31 13:49:50 +0100

    update package

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,6 @@
 import { defineUserConfig } from "vuepress";
+import { defaultTheme } from '@vuepress/theme-default'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 import theme from "./theme.js";
 
@@ -12,6 +14,12 @@ export default defineUserConfig({
       description: "A docs demo for vuepress-theme-hope",
     }
   },
+
+  plugins: [
+    docsearchPlugin({
+      // options
+    }),
+  ],
 
   theme,
 
